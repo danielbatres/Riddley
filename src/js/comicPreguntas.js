2d941868d4498ff33cc9edab6a97d99a7c1fc178
danielbatres <danielbatresuser@gmail.com>
2022-07-13 20:57:16 -0600

    Preguntas

--- src/js/comicPreguntas.js
+++ src/js/comicPreguntas.js
@@ -1,10 +1,14 @@
 import { Pregunta } from "./Pregunta.js";
 
-const pregunta1 = new Pregunta("Este es el enunciado de la primer pregunta de los comics", "Esta es la respuesta correcta 1", "Esta es la respuesta incorrecta 1");
-const pregunta2 = new Pregunta("Este es el enunciado de la segunda pregunta de los comics", "Esta es la respuesta correcta 2", "Esta es la respuesta incorrecta 2");
-const pregunta3 = new Pregunta("Este es el enunciado de la tercer pregunta de los comics", "Esta es la respuesta correcta 3", "Esta es la respuesta incorrecta 3");
-const pregunta4 = new Pregunta("Este es el enunciado de la cuarta pregunta de los comics", "Esta es la respuesta correcta 4", "Esta es la respuesta incorrecta 4");
-const pregunta5 = new Pregunta("Este es el enunciado de la quinta pregunta de los comics", "Esta es la respuesta correcta 5", "Esta es la respuesta incorrecta 5");
-const pregunta6 = new Pregunta("Este es el enunciado de la sexta pregunta de los comics", "Esta es la respuesta correcta 6", "Esta es la respuesta incorrecta 6");
+const pregunta1 = new Pregunta("Son los recuadros que separan las escenas", "Viñeta", "Imagen");
+const pregunta2 = new Pregunta("La palabra que imita un sonido se conoce con el nombre de...", "Onomatopeya", "Globo");
+const pregunta3 = new Pregunta("Son los que dan vida a la historia, en ellos se centra el relato", "Personajes", "Onomatopeya");
+const pregunta4 = new Pregunta("El bocadillo es", "Es el espacio donde se colocan los textos que piensan o dicen los personajes", "Es la unidad mínima de significación de una historieta");
+const pregunta5 = new Pregunta("¿El cómic requiere de pocos cuadros por qué?", "Sus imágenes son bastante explícitas.", "No necesita guardar secuencia crónologica");
+const pregunta6 = new Pregunta("Que opción NO corresponde a los elementos del comic", "Lenguaje audiovisual", "Signos convencionales");
+const pregunta7 = new Pregunta("¿Cómo se originaron los comics?", "Cuando el ser humano tuvo la necesidad de dibujar para poder explicar una historia aventura acontecimiento leyenda", "Una época y un espacio en los que la escritura no se concibe como un libro de leyes sagradas");
+const pregunta8 = new Pregunta("¿A quién se le considera el creador de los cómics?", "Tomas Rowslandson", "Richard F. Outcault");
+const pregunta9 = new Pregunta("¿Es considerado el primer cómic publicado en el mundo?", "The yellow Kid", "Detective comics");
+const pregunta10 = new Pregunta("¿Que aporte dieron los cómics?", "Favorecen la iniciación en la lectura de un modo ágil, divertido y atractivo", "Puede lograr que los alumnos se sientan identificados y comprendidos por otros sujetos de distintas características a las de ellos");
 
-export const preguntasComic = [pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6];
+export const preguntasComic = [pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6, pregunta7, pregunta8, pregunta9, pregunta10];
--- src/js/litPreguntas.js
+++ src/js/litPreguntas.js
@@ -1,9 +1,15 @@
 import { Pregunta } from "./Pregunta.js";
 
-const pregunta1 = new Pregunta("Este es el enunciado de la primer pregunta de la literatura salvadoreña", "Esta es la respuesta correcta 1", "Esta es la respuesta incorrecta 1");
-const pregunta2 = new Pregunta("Este es el enunciado de la segunda pregunta de la literatura salvadoreña", "Esta es la respuesta correcta 2", "Esta es la respuesta incorrecta 2");
-const pregunta3 = new Pregunta("Este es el enunciado de la tercer pregunta de la literatura salvadoreña", "Esta es la respuesta correcta 3", "Esta es la respuesta incorrecta 3");
-const pregunta4 = new Pregunta("Este es el enunciado de la cuarta pregunta de la literatura salvadoreña", "Esta es la respuesta correcta 4", "Esta es la respuesta incorrecta 4");
-const pregunta5 = new Pregunta("Este es el enunciado de la quinta pregunta de la literatura salvadoreña", "Esta es la respuesta correcta 5", "Esta es la respuesta incorrecta 5");
+const pregunta1 = new Pregunta("Se consideran los fundadores de la Literatura Salvadoreña", "Francisco Gavídia, Arturo Ambrogi y Alberto Masferrer", "Manlio Argueta. Salarrué, Roque Dalton");
+const pregunta2 = new Pregunta(`La obra "Andanzas y Malandanzas" pertenece al costumbrismo salvadoreño`, "Verdadero", "Falso");
+const pregunta3 = new Pregunta(`Escritor salvadoreño autor de diversos ensayos como: "El dinero maldito", "Minimun Vital", "Leer y escribir"`, "Alberto Masferrer", "Alfredo Espino");
+const pregunta4 = new Pregunta("¿Quién de los siguientes escritores es seguramente de los más aprovechados discipulos que tuvo Rubén Darío en El Salvador?", "Ramon de Nufio", "Alberto Rivas Bonilla");
+const pregunta5 = new Pregunta("Cuál es una característica de la literatura salvadoreña", "Descripciones extensas de los paisajes", "Uso excesivo y recargo de los recursos literarios");
+const pregunta6 = new Pregunta("Quien escribió: El extraño habitante", "Alvaro Menen Desleal", "Francisco Gavidia");
+const pregunta7 = new Pregunta("Su trabajo literario se enfoco en la denuncia de la injusticia social", "Alberto Masferrer", "Salarrué")
+const pregunta8 = new Pregunta("Son representantes de la generación del 44", "Pedro Geoffroy Rivas, José María Méndez Oswaldo Escobar Velado", "Gabriel García Márquez, Pablo Neruda, José Martí");
+const pregunta9 = new Pregunta("¿Hugo Lindo formó parte de la generación del 44?", "Verdadero", "Falso");
+const pregunta10 = new Pregunta(`¿Manlio Argueta es autor de la obra "Una grieta en el agua"?`, "Falso", "Verdadero");
+const pregunta11 = new Pregunta("¿Matilde Elena López pertenece a la generación literaria?", "Generación comprometida", "Generación del 44");
 
-export const preguntasLiteratura = [pregunta1, pregunta2, pregunta3, pregunta4, pregunta5];
+export const preguntasLiteratura = [pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6, pregunta7, pregunta8, pregunta9, pregunta10, pregunta11];
